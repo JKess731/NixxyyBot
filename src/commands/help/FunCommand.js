5f85524fa748a30f25d049a8c665908a1fa65847
JKess731 <62125526+JKess731@users.noreply.github.com>
2020-07-15 17:37:56 -0400

    1.0.3d-1
    added &fun panel

---
+++ src/commands/help/FunCommand.js
@@ -0,0 +1,21 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class FunCommand extends BaseCommand {
+  constructor() {
+    super('fun', 'fun', []);
+  }
+
+  run(client, message, args) {
+    const embed = new MessageEmbed()
+    .setTitle(`NixxyyBot | Fun Panel`)
+    .setColor(9577983)
+    .setAuthor(`${client.user.username} ${packageJson.version}`, client.user.displayAvatarURL())
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.username}`)
+    .addFields(
+      {name: "Yeah Yeah", value: "`&yeahyeah`\nShow your support for Nathan Davis Jr", inline:true},
+      )
+    message.channel.send(embed);
+  }
+}
